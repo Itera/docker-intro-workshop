7975de0a136446b6163b4cc930274ee35ef83942
Ole-Petter Olsen <opolsen@gmail.com>
2020-01-23 19:18:57 +0100

    Add python API

---
+++ api/api.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from random import randint
+
+values = [ 'FOO', 'BAR', 'BAZ' ]
+
+app = Flask(__name__)
+api = Api(app)
+
+class RandomValue(Resource):
+    def get(self):
+        return values[randint(0, len(values) - 1)]
+
+api.add_resource(RandomValue, '/value')
+
+if __name__ == '__main__':
+    app.run(debug=True)
