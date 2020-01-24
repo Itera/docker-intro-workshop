cbf6b81a6310187daedc10818031604f2e84ec7f
Ole-Petter Olsen <opolsen@gmail.com>
2020-01-24 13:53:14 +0100

    Enable CORS for python api

--- api/api.py
+++ api/api.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Resource, Api
+
 from random import randint
 
 values = [ 'FOO', 'BAR', 'BAZ' ]
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class RandomValue(Resource):
